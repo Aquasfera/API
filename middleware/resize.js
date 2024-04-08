12f566087b1a74dc97ddaf33d58b310deece917c
KevinFlorenciaMoragas <kevinflomor@campus.monlau.com>
2024-04-08 18:57:55 +0200

    08/04
    Ya se pueden subir fotos en 720p

---
+++ middleware/resize.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const multer = require('multer');
+const SharpMulter = require('sharp-multer');
+
+const storage = SharpMulter({
+    destination: (req, file, callback) => callback(null, './data/photos'),
+    imageOptions: {
+        fileFormat: 'jpg',
+        quality: 80,
+        resize: {
+            width: 1280, height: 720,
+        }
+    }
+})
+const upload = multer({ storage })
+
+module.exports = upload.single('url')
--- routes/photo.routes.js
+++ routes/photo.routes.js
@@ -10,11 +10,14 @@ const {
     readItem
 } = require('../controllers/generics.controller.js')
 const checkToken = require('../middleware/token.js')
-
-router.get('/photo', async(req,res) => await readAllItems(req,res,Photo))
-router.get('/photo/:id', async(req,res) => await readItem(req,res,Photo))
-router.post('/photo', checkToken, async(req,res) => await createItem(req,res,Photo))
-router.put('/photo/:id', checkToken, async(req,res) => await updateItem(req,res,Photo))
-router.delete('/photo/:id', checkToken, async(req,res) => await deleteItem(req,res,Photo))
+const resize = require('../middleware/resize.js')
+router.get('/photo', async (req, res) => await readAllItems(req, res, Photo))
+router.get('/photo/:id', async (req, res) => await readItem(req, res, Photo))
+router.post('/photo', [resize,checkToken], async (req, res) => {
+    req.body.url = req.file.path
+    await createItem(req, res, Photo)
+})
+router.put('/photo/:id', checkToken, async (req, res) => await updateItem(req, res, Photo))
+router.delete('/photo/:id', checkToken, async (req, res) => await deleteItem(req, res, Photo))
 
 module.exports = router
