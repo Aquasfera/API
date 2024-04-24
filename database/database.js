c4d612fbc8d5a6cd718f838dd5d8d007dcae8bfc
KevinFlorenciaMoragas <kevinflomor@campus.monlau.com>
2024-04-24 19:00:43 +0200

    25

--- database/database.js
+++ database/database.js
@@ -2,6 +2,8 @@ const {Sequelize, Datatypes} = require('sequelize')
 
 const sequelize = new Sequelize("aquaesfera","root","monlau21!",{
     host:"localhost",
-    dialect:"mysql"
+    dialect:"mysql",
+    port:3306,
+    
 })
 module.exports = sequelize
